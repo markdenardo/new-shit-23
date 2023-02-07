4046f1bc2c87bc79168d8f22ad5d1853540555bb
MDN <mark.denardo@gmail.com>
2023-02-06 20:56:30 -0500

    Update gojira.js

--- gojira.js
+++ gojira.js
@@ -1,19 +1,15 @@
 //gojira
 //mdn 2023
+s0.initVideo("https://64.media.tumblr.com/aeadd8c6abf2914b70a698c092ec3c4d/tumblr_n5gqc7rPS01qcga5ro1_500.mp4")
 shape(3,0.1,0.1)
   .rotate(1,1)
   .repeat(2,1)
   .pixelate(100,100)
   .out(o0)
 render(o0)
-
-s0.initVideo("https://64.media.tumblr.com/aeadd8c6abf2914b70a698c092ec3c4d/tumblr_n5gqc7rPS01qcga5ro1_500.mp4")
-	src(s0).out(o1)
-
 src(o0)
-  .blend(o1)
+  .blend(s0)
   .add(osc(1,1,1)
        .color(1,0,0))
   .out(o2)
-
 render(o2)
